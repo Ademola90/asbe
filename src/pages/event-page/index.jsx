8e7347776de408c0b08773489f6ea74abf922749
Ademola90 <ademolaabdullahi989@gmail.com>
2023-11-04 21:54:26 +0100

    Event-page

--- src/pages/event-page/index.jsx
+++ src/pages/event-page/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import EventEroimg from "../../assets/EvenHerotimg.png";
 
 const Events = () => {
   return (
-    <div className=" mt-32">
-      <div>Events</div>
+    <div className=" mt-24 mx-auto overflow-x-hidden">
+      <div
+        className=" bg-no-repeat bg-cover bg-center h-screen grid items-center"
+        style={{
+          backgroundImage: `url(${EventEroimg})`,
+        }}
+      >
+        <div className=" grid items-center text-white gap-10 lg:px-16 md:px-10 px-5">
+          <p className=" lg:text-[64px] md:text-[84px] text-[50px] font-bold font-satoshi lg:w-[600px] md:w-[600px] ">
+            ASBE GLOBAL INNOVATIONS{" "}
+          </p>
+          <p className="lg:w-[500px] text-[24px]">
+            Technology and skills developers across higher institution of
+            learning in Africa.
+          </p>
+        </div>
+      </div>
+
+      {/* UPCOMING EVENTS/PROGRAMS */}
     </div>
   );
 };
